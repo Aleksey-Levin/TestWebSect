bcbde955c4b76685def6a0c868e06b49f0b3a6c1
AlekseyLevin <66261148+Aleksey-Levin@users.noreply.github.com>
2021-08-25 05:30:43 +0800

    Add files via upload

--- VstupitIS/main.js
+++ VstupitIS/main.js
@@ -3,76 +3,60 @@ const button_add = document.querySelector('#todo_button_add');
 const div = document.querySelector('.box');
 const button_save = document.querySelector('#todo_button_save');
 
-// <-- Создаём счётчик на замыкании для генерации id -->
-const createIDFunction = (arg) =>{
-    let current = 0;
-    return function () {
-        current++
-        return String(current) + arg;
-    }
-}
-
-// <-- Создаём функции для генерации id по переданному аргументу -->
-const createIdButton = createIDFunction('button')
-const createIdDiv = createIDFunction('div')
-const createIdCheck = createIDFunction('check')
 
 // <-- Основное слушание кнопки "Add" -->
-button_add.addEventListener('click',()=>{
+button_add.addEventListener('click', () => {
 
-// <-- Генерация id для данного запроса -->
-    const buttonId = createIdButton();
-    const divId = createIdDiv();
-    const checkId = createIdCheck();
 
 // <-- Считывание данных с инпутов -->
     let value = document.querySelector('#todo_input').value;
-    const valueName = document.querySelector('#todo_input_name').value!='' ? document.querySelector('#todo_input_name').value : 'Неизвестный пользователь' ;
+    const valueName = document.querySelector('#todo_input_name').value;
 
 // <-- Проверка на заполненность инпута -->
-    if(value!=='' ){
-    value = `${valueName}: ${value}`;
+    if (value !== '' && valueName !== '') {
+        value = `${valueName}: ${value}`;
 
-    div.insertAdjacentHTML('beforeend', `
-        <div class = 'under_input' id=${divId} >
-            <div class="check none" id = ${checkId} ></div>
-            <div class="under_input_text">
-                ${value} 
-            </div>
-            <button class = "todo_button todo_button_del" id=${buttonId}>delete</button>
+        div.insertAdjacentHTML('beforeend', `
+        <div class = 'under_input'>
+            <div class="check none"></div>
+            <div class="under_input_text">${value}</div>
+            <button class = "todo_button todo_button_del">delete</button>
         </div>
     `)
-    // <-- Инициализация кнопки "Delete" и навешивание слушателея -->
-    const button_delete = document.getElementById(buttonId);
-    button_delete.addEventListener('click', ()=>{
-        const divDelete = document.getElementById(divId)
-        divDelete.remove();
-    })
-    // <-- Инициализация кнопки для отметки статуса выполнения -->
-    const check = document.getElementById(checkId);
-    check.addEventListener('click',(item)=>{
-        id = item.target.id;
-        const currentItem = document.getElementById(id);
-        currentItem.classList.toggle("none");
-        currentItem.classList.toggle("clicked");
-    })
+        // <-- Инициализация кнопки "Delete" и навешивание слушателея -->
+        let button_delete = document.querySelectorAll('.todo_button_del');
+        button_delete = button_delete[button_delete.length-1]
+        button_delete.addEventListener('click', (item) => {
+            item.path[1].remove()
+
+        })
+
+        // <-- Инициализация кнопки для отметки статуса выполнения -->
+        let  check = document.querySelectorAll('.check');
+        check = check[check.length-1]
+        check.addEventListener('click', (item) => {
+            item.target.classList.toggle("none");
+            item.target.classList.toggle("clicked");
+        })
     }
 })
 
 // <-- Слушатель на кнопку "Save"
-button_save.addEventListener('click',()=>{
-    obj = {
+button_save.addEventListener('click', () => {
+    let obj = {
         todoTask: []
     }
 
     const arrayNodes = document.querySelectorAll('.under_input')
-    arrayNodes.forEach((item)=>{
-        tempObj = new Object();
-        const text = item.childNodes[3].innerText.split(':').map(item=>{return item.replace(/ /g,'')})
+    arrayNodes.forEach((item) => {
+        let tempObj = {};
+        const text = item.childNodes[3].innerText.split(':').map(item => {
+            return item.replace(': ', '')
+        })
+        console.log(text)
         tempObj.name = text[0];
         tempObj.text = text[1];
-        const flag = item.childNodes[1].classList.contains('clicked');
-        tempObj.check = flag;
+        tempObj.check = item.childNodes[1].classList.contains('clicked');
         obj.todoTask.push(tempObj);
     })
 
